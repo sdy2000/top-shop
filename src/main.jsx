97eeb3226d4a2864bac7e966007f6db8356e11c3
atabak-jamshidi <atabak.jamshidi@outlook.com>
2023-07-30 12:41:41 +0330

    header come to finish

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./assets/global.css";
 import { Provider } from "react-redux";
 import { store } from "./context/store.js";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
