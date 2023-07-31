e911d5eba8ff2abc3aa0e59657f8729aa7ba946b
sajjad <sajjad.darvish.yektayi@gmail.com>
2023-08-01 01:47:09 +0430

    Update footer and Create footer-diff-language data

--- src/features/layouts/layout/data/index.js
+++ src/features/layouts/layout/data/index.js
@@ -1,3 +1,4 @@
 export { default as currency } from "./currency";
 export { default as language } from "./language";
 export { default as barDiffLanguage } from "./bar-diff-language";
+export { default as footerDiffLanguage } from "./footer-diff-language";
--- src/features/layouts/layout/footer.jsx
+++ src/features/layouts/layout/footer.jsx
@@ -1,93 +1,96 @@
+import { useSelector } from "react-redux";
+import { footerDiffLanguage } from "./data";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <footer>
-    <div className="widgets">
-      <div className="container">
-        <div className="wrapper">
-          <div className="flexwrap">
-            <div className="row">
-              <div className="item mini-links">
-                <h4>Help & Contact</h4>
-                <ul className="flexcol">
-                  <li><a href="">Your Account</a></li>
-                  <li><a href="">Your Orders</a></li>
-                  <li><a href="">Shipping Rates</a></li>
-                  <li><a href="">Returns</a></li>
-                  <li><a href="">Assistant</a></li>
-                  <li><a href="">Help</a></li>
-                  <li><a href="">Contact us</a></li>
-                </ul>
-              </div>
-            </div>
-            <div className="row">
-              <div className="item mini-links">
-                <h4>Product Categories</h4>
-                <ul className="flexcol">
-                  <li><a href="">Beauty</a></li>
-                  <li><a href="">Electronic</a></li>
-                  <li><a href="">Womens Fashion</a></li>
-                  <li><a href="">Mens Fashion</a></li>
-                  <li><a href="">Girls Fashion</a></li>
-                  <li><a href="">Boys Fashion</a></li>
-                  <li><a href="">Health & Household</a></li>
-                  <li><a href="">Home & Kitchen</a></li>
-                  <li><a href="">Pet Supplies</a></li>
-                  <li><a href="">Sports</a></li>
-                </ul>
-              </div>
-            </div>
-            <div className="row">
-              <div className="item mini-links">
-                <h4>Payment Info</h4>
-                <ul className="flexcol">
-                  <li><a href="">Bussiness Card</a></li>
-                  <li><a href="">Shop with Points</a></li>
-                  <li><a href="">Reload Your Balance</a></li>
-                  <li><a href=""></a>Paypal</li>
-                </ul>
-              </div>
-            </div>
-            <div className="row">
-              <div className="item mini-links">
-                <h4>About Us</h4>
-                <ul className="flexcol">
-                  <li><a href="">Company Info</a></li>
-                  <li><a href="">News</a></li>
-                  <li><a href="">Investors</a></li>
-                  <li><a href="">Careers</a></li>
-                  <li><a href="">Policies</a></li>
-                  <li><a href="">Customer reviews</a></li>
-                </ul>
-              </div>
+  const currentLanguage = useSelector((store) => store.language);
+
+  return (
+    <footer>
+      <div className="widgets">
+        <div className="container">
+          <div className="wrapper">
+            <div className="flexwrap">
+              {footerDiffLanguage.map((cul, idx) => (
+                <div className="row" key={idx}>
+                  <div className="item mini-links">
+                    <h4>
+                      {
+                        cul.header.find(
+                          (hed) => hed.lan === currentLanguage.symbol
+                        ).text
+                      }
+                    </h4>
+                    <ul className="flexcol">
+                      {cul.categories.map((cat, idx1) => {
+                        let title = cat.title.find(
+                          (tit) => tit.lan === currentLanguage.symbol
+                        ).text;
+                        return (
+                          <li key={idx1}>
+                            <Link to={cat.slug} title={title}>
+                              {title}
+                            </Link>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
-    </div>
-    {/*// <!--   Widgets -->*/}
+      {/*// <!--   Widgets -->*/}
 
-    <div className="footer-info">
-      <div className="container">
-        <div className="wrapper">
-          <div className="flexcol">
-            <div className="logo">
-              <a href=""><span className="circle"></span>.Store</a>
+      <div className="footer-info">
+        <div className="container">
+          <div className="wrapper">
+            <div className="flexcol">
+              <div className="logo">
+                <a href="">
+                  <span className="circle"></span>.Store
+                </a>
+              </div>
+              <div className="socials">
+                <ul className="flexitem">
+                  <li>
+                    <a href="">
+                      <i className="ri-twitter-line"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <i className="ri-facebook-line"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <i className="ri-instagram-line"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <i className="ri-linkedin-line"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <i className="ri-youtube-line"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="socials">
-              <ul className="flexitem">
-                <li><a href=""><i className="ri-twitter-line"></i></a></li>
-                <li><a href=""><i className="ri-facebook-line"></i></a></li>
-                <li><a href=""><i className="ri-instagram-line"></i></a></li>
-                <li><a href=""><i className="ri-linkedin-line"></i></a></li>
-                <li><a href=""><i className="ri-youtube-line"></i></a></li>
-              </ul>
-            </div>
+            <p className="mini-text">
+              Copyright 2023 .Store. All right reserved.
+            </p>
           </div>
-          <p className="mini-text">Copyright 2023 .Store. All right reserved.</p>
         </div>
       </div>
-    </div>
-    {/*// <!--   Footer Info -->*/}
-
-  </footer>;
+      {/*// <!--   Footer Info -->*/}
+    </footer>
+  );
 };
 export default Footer;
