9853752bac1abb048f83906863c0a03386df71ea
atabak-jamshidi <atabak.jamshidi@outlook.com>
2023-07-31 22:21:35 +0330

    top footer added

--- src/features/layouts/home-layout/top-footer.jsx
+++ src/features/layouts/home-layout/top-footer.jsx
@@ -1,4 +1,22 @@
 const TopFooter = () => {
-  return <div>TopFooter</div>;
+  return <div>
+    <div className="newsletter">
+      <div className="container">
+        <div className="wrapper">
+          <div className="box">
+            <div className="content">
+              <h3>Join Our Newsletter</h3>
+              <p>Get E-mail Updates about our latest shop and <strong>special offers</strong></p>
+            </div>
+            <form action="" className="search">
+              <span className="icon-large"><i className="ri-mail-line"></i></span>
+              <input type="email" placeholder="Your email address" required />
+                <button type="submit">Sign Up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 export default TopFooter;
